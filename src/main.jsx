6ab85025ebcbf171e2e8e7dd9a296fd1cebe4389
minhazapon <minhajulabedinapon@gmail.com>
2024-06-19 04:51:30 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root';
+import Home from './Home';
+import Nav from './Nav';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element:  <Home></Home>,
+      },
+      {
+        path: "/nav",
+        element:   <Nav></Nav>,
+      },
+      
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
